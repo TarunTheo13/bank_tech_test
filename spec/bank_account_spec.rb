fe35fdfad8119c13137fe4a8f9803035266937be
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-08-02 22:17:17 +0100

    initializes with a balance of zero

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,5 +1,9 @@
 require 'bank_account'
 
 describe BankAccount do
-  
+  describe '#initialize' do
+    it 'initializes with a bank balance of zero' do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
