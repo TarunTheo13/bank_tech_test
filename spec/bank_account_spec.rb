51752625ab169a426566eb72e19975b345ee2ccd
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-08-02 22:25:05 +0100

    can alter bank balance by depositing and withdrawing money

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -6,4 +6,20 @@ describe BankAccount do
       expect(subject.balance).to eq 0
     end
   end
+
+  describe '#deposit' do
+    it 'increases the balance when money is deposited' do
+      subject.deposit(500)
+      expect(subject.balance).to eq 500
+    end
+  end
+
+  describe '#withdraw' do
+    it 'decreases the balance when money is withdrawn' do
+      subject.deposit(500)
+      subject.withdraw(300)
+      expect(subject.balance).to eq 200
+    end
+  end
+
 end
